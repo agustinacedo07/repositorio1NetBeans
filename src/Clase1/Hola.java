407d64457a8e085f1dc170666a7f2758c5b9033d
ponaquitunombre <ponaquitunombre@1DAM-O01>
2018-03-15 13:25:09 +0100

    Commit 1. Bajada del proyecto del repostorio al local.

--- src/Clase1/Hola.java
+++ src/Clase1/Hola.java
@@ -7,6 +7,7 @@ public class Hola {
         System.out.println("Hola Caracola");
         System.out.println("Version Nueva ");
         System.out.println("Hola soy Pepito");
+        System.out.println("Hasta luego Lucaa");
     }
     
 }
